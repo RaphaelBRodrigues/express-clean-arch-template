acc4fb3b4e01fef1639823b51fa0b6e82e0ffdf6
RaphaelBRodrigues <raphaelbarbosa.rodrigues@gmail.com>
2023-07-23 20:21:16 -0300

    fix: removing participants routes

--- backend/src/infra/db/sequelize/repositories/participant.repository.js
+++
@@ -1,9 +0,0 @@
-const ParticipantRepository = require("../../../../domain/repositories/participant.repository");
-
-class ParticipantSequelizeRepository extends ParticipantRepository {
-  async findById(id) {
-    return {}
-  }
-}
-
-module.exports = ParticipantSequelizeRepository;
--- backend/src/infra/http/express/http-server.js
+++ backend/src/infra/http/express/http-server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const ParticipantRoutes = require('./routes/participant.routes');
 const ElectionRoutes = require('./routes/election.routes');
 const SwaggerRoutes = require('./routes/swagger.routes');
 
@@ -14,18 +13,17 @@ class HttpServer {
   }
 
   createRoutes() {
-    const participantRoutes = new ParticipantRoutes().routes;
     const electionRoutes = new ElectionRoutes().routes;
     const swaggerRoutes = new SwaggerRoutes().routes;
 
     this.app.use(express.json())
 
     this.app.use("/elections", electionRoutes);
-    this.app.use("/participants", participantRoutes);
     this.app.use("/swagger", swaggerRoutes);
 
     this.app.use((error, __, res, ___) => {
-      console.error(error)
+      console.error(error);
+
       return res.status(500).json({
         message: "internal server error"
       })
