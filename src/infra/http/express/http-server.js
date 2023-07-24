bfdf8575c2174904529396bdcbfb2b58e9545705
Raphael Barbosa Rodrigues <raphaelbarbosa.rodrigues@gmail.com>
2023-07-23 23:15:54 -0300

    allowing cors

--- src/infra/http/express/http-server.js
+++ src/infra/http/express/http-server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ElectionRoutes = require('./routes/election.routes');
 const SwaggerRoutes = require('./routes/swagger.routes');
 const cors = require('cors');
+const path = require('path');
 
 class HttpServer {
   constructor() {
@@ -9,20 +10,29 @@ class HttpServer {
   }
 
   start() {
+    this.setupMiddlewares();
     this.createRoutes();
     this.startListener();
+    this.setupErrorHandler();
+  }
+
+  setupMiddlewares() {
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.static(
+      path.resolve(__dirname, "../../resources/images")
+    ));
   }
 
   createRoutes() {
     const electionRoutes = new ElectionRoutes().routes;
     const swaggerRoutes = new SwaggerRoutes().routes;
 
-    this.app.use(cors());
-    this.app.use(express.json())
-
     this.app.use("/elections", electionRoutes);
     this.app.use("/swagger", swaggerRoutes);
+  }
 
+  setupErrorHandler() {
     this.app.use((error, __, res, ___) => {
       console.error(error);
 
